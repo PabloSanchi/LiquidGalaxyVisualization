884ca3fc7d467ae89a998f4bcef74d68cbd179b4
Pablo <pablo.sanchi.herrera@gmail.com>
2022-07-05 14:30:46 +0200

    bug fix

--- public/multiple.js
+++ public/multiple.js
@@ -11,6 +11,7 @@ let startX = 0;
 let startY = 0;
 let camera;
 
+
 socket.on('updateScreen', (coords) => {
     if (screen == 1) return; // do not update the master screen
 
@@ -18,7 +19,10 @@ socket.on('updateScreen', (coords) => {
 });
 
 
-// on socket connect; JUST ONCE
+/*
+update: on first connection, retrieve data, screen number
+    and send essential data (to the server) like the size (screen id, screen width, screen height)
+*/
 socket.on('update', (screenData) => {
     if (done) return;
     document.title = screenData.id;
@@ -36,25 +40,36 @@ socket.on('update', (screenData) => {
 });
 
 
-
+/*
+Start visulization when the server gives the signal to do so
+    - Retrieve: - super-resuloution (the total width of the screens)
+                - Calculate the portion to the screen
+    
+    @param {Object} superRes, contains {width, height, child(Object)}
+        child (Object): {1: width, 2: width, 3: width, ..., n: width}
+*/
 socket.on('start', (superRes) => {
     console.log('screen' + screen + ' ready');
     
-
+    // super resolution width and height
     fullWidth = superRes.width;
     fullHeight = window.innerHeight;
 
     // calculate each screen startX
     let scRes = superRes.child;
 
+    // master is 0
+    // left screens have negative values (negative offset)
+    // right screens have positive values (positive offset)
     startX = 0;
-    // right side screen
+
+    // right side screens
     if(screen % 2 == 0) {
         startX = scRes[1];
         for(let index = 2; index < screen; index+=2) {
             startX += scRes[index];
         }
-    }else {
+    }else { // left side screens
         for(let index = 3; index <= screen; index+=2) {
             startX -= scRes[index];
         }
@@ -65,11 +80,16 @@ socket.on('start', (superRes) => {
     console.log('superRes: (' + fullWidth + ',' + fullHeight + ')');
     console.log('StartX: ' + startX + ' StartY: ' + startY);
 
+    // start animation
     init();
     animate();
 });
 
 
+/*
+UpdateMouse -> update mouse position, only works for slaves
+@param {Object} mouse, mouse {mousex: value, mousey: value}
+*/
 socket.on('updateMouse', (mouse) => {
     if (screen == 1) return;
     console.log('up');
@@ -78,10 +98,13 @@ socket.on('updateMouse', (mouse) => {
     mouseY = mouse.mousey;
 });
 
-
+/*
+UpdateMouse -> update camera position z, only works for slaves
+@param {Object} pos: {z: value}
+*/
 socket.on('updatePosScreen', (pos) => {
     if (screen == 1) return;
-    camera.position.z = pos.x;
+    camera.position.z = pos.z;
 });
 
 window.onload = function () {
@@ -92,23 +115,19 @@ const onDocumentKeyDown = (event) => {
 
     if (screen != 1) return;
 
-    console.log('ZOOM');
     var keyCode = event.which;
     if (keyCode == 87) { // w
         camera.position.z += 100;
     } else if (keyCode == 83) { // s
         camera.position.z -= 100;
-    // } else if (keyCode == 65) { // a
-    //     camera.position.x += 5;
-    // } else if (keyCode == 68) { // d
-    //     camera.position.x -= 5;
     } else return
 
     socket.emit('updatePos', {
-        x: camera.position.z
+        z: camera.position.z
     });
 }
 
+// variables
 const views = [];
 let scene, renderer;
 let mouseX = 0, mouseY = 0;
@@ -116,6 +135,18 @@ const windowHalfX = window.innerWidth / 2;
 const windowHalfY = window.innerHeight / 2;
 
 
+/*
+View - set the camera offset according to the screen dimensions and position (...5,3,1,2,4...)
+
+@param {Canvas} canvas
+@param {Number} fullWidth
+@param {Number} fullHeight
+@param {Number} viewX
+@param {Number} viewY
+@param {Number} viewWidth
+@param {Number} viewHeight
+
+*/
 function View(canvas, fullWidth, fullHeight, viewX, viewY, viewWidth, viewHeight) {
 
     canvas.width = viewWidth * window.devicePixelRatio;
@@ -127,14 +158,15 @@ function View(canvas, fullWidth, fullHeight, viewX, viewY, viewWidth, viewHeight
 
     // Orthographic Camera
     // camera = new THREE.OrthographicCamera
-    //     (-viewWidth / 4, viewWidth, viewHeight, -viewHeight, 1, 10000);
+    //     (-viewWidth, viewWidth, viewHeight, -viewHeight, 1, 10000);
 
-    // set camera first adjustements
+    // set camera offset
     camera.setViewOffset(fullWidth, fullHeight, viewX, viewY, viewWidth, viewHeight);
-    camera.position.z = 1000; // 1800
+    
+    // camera default position
+    camera.position.z = 1000; // default camera z index pos
     camera.position.x += (mouseX - camera.position.x) * 0.05;
     camera.position.y += (- mouseY - camera.position.y) * 0.05;
-    // camera.lookAt( scene.position );
 
     this.render = function () {
 
@@ -146,7 +178,6 @@ function View(canvas, fullWidth, fullHeight, viewX, viewY, viewWidth, viewHeight
         renderer.render(scene, camera);
 
         context.drawImage(renderer.domElement, 0, 0);
-
     };
 }
 
@@ -172,67 +203,6 @@ function init() {
     light.position.set(0, 0, 1).normalize();
     scene.add(light);
 
-    const canvas = document.createElement('canvas');
-    canvas.width = 128;
-    canvas.height = 128;
-
-    // var geometry = new THREE.BoxGeometry( 10, 10, 10 );
-    // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00, wireframe: true } );
-    // cube = new THREE.Mesh( geometry, material );
-    // cube.position.set(0,0,0);
-    // scene.add( cube );
-
-    
-    // EXAMPLE ICOSAEDRON
-
-    // const context = canvas.getContext('2d');
-    // const gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
-    // gradient.addColorStop(0.1, 'rgba(210,210,210,1)');
-    // gradient.addColorStop(1, 'rgba(255,255,255,1)');
-
-    // context.fillStyle = gradient;
-    // context.fillRect(0, 0, canvas.width, canvas.height);
-
-    // const noof_balls = 10;
-    // const radius = 100;
-
-    // const geometry1 = new THREE.IcosahedronGeometry(radius, 1);
-
-    // const count = geometry1.attributes.position.count;
-    // geometry1.setAttribute('color', new THREE.BufferAttribute(new Float32Array(count * 3), 3));
-
-    // const color = new THREE.Color();
-    // const positions = geometry1.attributes.position;
-    // const colors = geometry1.attributes.color;
-
-    // for (let i = 0; i < count; i++) {
-    //     color.setHSL((positions.getY(i) / radius + 1) / 2, 1.0, 0.5);
-    //     colors.setXYZ(i, color.r, color.g, color.b);
-    // }
-
-    // const material = new THREE.MeshPhongMaterial({
-    //     color: 0xffffff,
-    //     flatShading: true,
-    //     vertexColors: true,
-    //     shininess: 0
-    // });
-
-    // const wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true, transparent: true });
-
-    // for (let i = 0; i < noof_balls; i++) { // create balls
-
-    //     const mesh = new THREE.Mesh(geometry1, material);
-    //     const wireframe = new THREE.Mesh(geometry1, wireframeMaterial);
-    //     mesh.add(wireframe);
-
-    //     mesh.position.x = - (noof_balls - 1) / 2 * 400 + i * 400;
-    //     mesh.rotation.x = i * 0.5;
-
-    //     scene.add(mesh);
-
-    // }
-
-    // END EXAMPLE ICOSAEDRON
 
     try {
         const loader = new THREE.GLTFLoader();
@@ -257,9 +227,7 @@ function init() {
             },
             // called when loading has errors
             function (error) {
-
                 console.log('An error happened');
-
             }
         );
     } catch(err) {
@@ -294,3 +262,59 @@ function animate() {
 
     requestAnimationFrame(animate);
 }
+
+
+// EXAMPLE ICOSAEDRON
+
+// const canvas = document.createElement('canvas');
+// canvas.width = 128;
+// canvas.height = 128;
+
+// const context = canvas.getContext('2d');
+// const gradient = context.createRadialGradient(canvas.width / 2, canvas.height / 2, 0, canvas.width / 2, canvas.height / 2, canvas.width / 2);
+// gradient.addColorStop(0.1, 'rgba(210,210,210,1)');
+// gradient.addColorStop(1, 'rgba(255,255,255,1)');
+
+// context.fillStyle = gradient;
+// context.fillRect(0, 0, canvas.width, canvas.height);
+
+// const noof_balls = 10;
+// const radius = 100;
+
+// const geometry1 = new THREE.IcosahedronGeometry(radius, 1);
+
+// const count = geometry1.attributes.position.count;
+// geometry1.setAttribute('color', new THREE.BufferAttribute(new Float32Array(count * 3), 3));
+
+// const color = new THREE.Color();
+// const positions = geometry1.attributes.position;
+// const colors = geometry1.attributes.color;
+
+// for (let i = 0; i < count; i++) {
+//     color.setHSL((positions.getY(i) / radius + 1) / 2, 1.0, 0.5);
+//     colors.setXYZ(i, color.r, color.g, color.b);
+// }
+
+// const material = new THREE.MeshPhongMaterial({
+//     color: 0xffffff,
+//     flatShading: true,
+//     vertexColors: true,
+//     shininess: 0
+// });
+
+// const wireframeMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true, transparent: true });
+
+// for (let i = 0; i < noof_balls; i++) { // create balls
+
+//     const mesh = new THREE.Mesh(geometry1, material);
+//     const wireframe = new THREE.Mesh(geometry1, wireframeMaterial);
+//     mesh.add(wireframe);
+
+//     mesh.position.x = - (noof_balls - 1) / 2 * 400 + i * 400;
+//     mesh.rotation.x = i * 0.5;
+
+//     scene.add(mesh);
+
+// }
+
+// END EXAMPLE ICOSAEDRON
